c97b5743971904e74a3694094051e813b95e7db2
Bryam Rodriguez <brodriguezaybar@gmail.com>
2017-05-23 17:35:47 -0500

    wip

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { styles } from './home.css';
 
 function Home() {
   return (
-    <h1>Bienvenido :) hola mundo</h1>
+    <h1 className={styles.title}>Bienvenido :) hola mundo</h1>
   );
 }
 
--- webpack/webpack.client.config.js
+++ webpack/webpack.client.config.js
@@ -25,7 +25,7 @@ const config = {
       {
         test: /\.css$/,
         exclude: /(node_modules)/,
-        loader: ExtractTextPlugin.extract({
+        use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [
            {
